3a442b1d314b3ee473e0eff8df9b6e5e03c296c5
stockulus <stockulus@icloud.com>
2016-08-14 10:45:01 +0200

    cleanup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Intl = require('intl')
 module.exports = function humanizeDistance (pointA, pointB, locale, unitSystem) {
   locale = locale || 'en-US'
   unitSystem = unitSystem || 'metric'
-  const toRad = (degree) => degree * Math.PI / 180
+  const toRad = degree => degree * Math.PI / 180
 
   const deltaLatitude = toRad(pointA.latitude - pointB.latitude)
   const deltaLongitude = toRad(pointA.longitude - pointB.longitude)
@@ -42,23 +42,3 @@ module.exports = function humanizeDistance (pointA, pointB, locale, unitSystem) 
 
   return `${formatter.format(result.distance)} ${result.unit}`
 }
-
-/*
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1);
-  var a =
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
-*/
